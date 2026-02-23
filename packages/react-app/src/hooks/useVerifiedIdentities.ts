e3b6109be5faeaadca39b6f65a6488ae0e71877c
Agnes Koinange <koinangeagness@gmail.com>
2026-02-24 02:53:10 +0300

    fix: handle verify multicall errors

--- packages/react-app/src/hooks/useVerifiedIdentities.ts
+++ packages/react-app/src/hooks/useVerifiedIdentities.ts
@@ -39,6 +39,8 @@ export const useVerifiedIdentities = (account: `0x${string}` | undefined) => {
         }).then(result => {
             const ids = { GoodID: !!result[0].result, WorldID: !!result[1].result, NoundsDAO: !!result[2].result, BrightID: !!result[3].result }
             setIdentities(ids)
+        }).catch(() => {
+            setIdentities({ GoodID: false, WorldID: false, NoundsDAO: false, BrightID: false })
         })
     }, [account])
 
